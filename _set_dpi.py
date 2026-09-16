7833212490f0e828d9598c1dcd931fe357f0c3f5
glabrousape4 <glabrousape4@gmail.com>
2026-09-17 08:29:00 +0900

    v89: 헤더+데이터를 단일 <table>로 통합, colgroup으로 컬럼 폭 고정 (perfect align)

--- _set_dpi.py
+++ _set_dpi.py
@@ -2,4 +2,6 @@ from PIL import Image
 img = Image.open(r"C:/Users/glabr/.cloudflared/sites/sejong/compare-output.png")
 img.save(r"C:/Users/glabr/.cloudflared/sites/sejong/compare-output.png", dpi=(300, 300), optimize=True)
 print("Size:", img.size)
-print("Bytes:", __import__('os').path.getsize(r"C:/Users/glabr/.cloudflared/sites/sejong/compare-output.png"))
+print("DPI:", Image.open(r"C:/Users/glabr/.cloudflared/sites/sejong/compare-output.png").info.get('dpi'))
+import os
+print("Bytes:", os.path.getsize(r"C:/Users/glabr/.cloudflared/sites/sejong/compare-output.png"))
